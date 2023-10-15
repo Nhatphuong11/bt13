5fae945420d7c3bdc36c3ac125a46e098b26ef1e
Nhatphuong11 <phuong201101@gmail.com>
2023-10-16 02:05:11 +0700

    first commit

---
+++ src/bt/bt1.java
@@ -0,0 +1,20 @@
+package bt;
+import java.util.Arrays;
+public class bt1 {
+    public static void main(String[] args) {
+        int[] arr = getArr();
+        System.out.println(Arrays.toString(arr));
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) max = arr[i];
+        }
+        System.out.println("Số lớn nhất trong mảng là: " + max);
+    }
+    private static int[] getArr(){
+        int arr[] = new int[20];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100);
+        }
+        return arr;
+    }
+}
